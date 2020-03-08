d4742a8c8988785b73597dbb80c9da0f588a5331
Estrella <egarcia2@wellesley.edu>
2020-03-08 15:26:49 -0400

    push1

--- s1c01.py
+++ s1c01.py
@@ -1,7 +1,10 @@
-
+import binascii as b
 
 def b64ToHex(b64String):
-    pass
+    binary_rep = b.a2b_base64(b64String)
+    return b.hexlify(binary_rep)
 
 def hexToB64(hexString):
     pass
+
+print(b64ToHex('SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'))
