6726702883f2e7b51db2960c2fc898f8e2a53c25
Gaby Inchoco <ginchoco@cs.wellesley.edu>
2020-03-10 14:47:57 -0400

    finished challenge 4

--- s1c04.py
+++ s1c04.py
@@ -1,6 +1,26 @@
+from s1c03 import *
+import sys
+import binascii as b
+#from s1c02 import xor
+
+def solveS1C04(fileStr):
+    f = open(fileStr, 'r')
+    if f.mode == 'r':
+        contents = f.readlines()
+
+        top_score = 0
+        pt = ""
+        for x in contents:
+            bytes_input = b.unhexlify(x.strip())
+            key = solveS1C03(bytes_input)
+            plaintext = caesarDecrypt(bytes_input, key)
+            current_score = scoreText(plaintext)
+            if current_score > top_score:
+                top_score = current_score
+                pt = plaintext
+                
+        return pt.strip()
+            
 
 
-
-def solveS1C04():
-    pass
 
