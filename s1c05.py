a65bc4b755fed4e0c3c88ebcae1e9208440b6d96
Isabelle Chun <isabelle.chun@gmail.com>
2020-03-11 12:35:16 -0400

    done with challenge 5

--- s1c05.py
+++ s1c05.py
@@ -1,11 +1,15 @@
-
+from s1c02 import xor
 
 
 def vigenereEncrypt(str):
-    # Extend ICE to the length of the input
-    key = "ICE"
-    long_key = len(str)//3
-    # XOR the long key with str
 
-def vigenereDecrypt():
-    pass
+    # Extend ICE to the length of the input
+    key = b"ICE"
+    key_multiples = (len(str)//3) + 1
+    key = key * key_multiples
+    key = key[0:(len(str))] # shorten to correct length
+    # XOR the long key with str
+    return xor(key, str)
+
+def vigenereDecrypt(str):
+    return vigenereEncrypt(str)
