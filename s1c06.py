a6cd9a9c57660bb2c79901cfa0e7c0ed73e60cbb
Gaby Inchoco <ginchoco@cs.wellesley.edu>
2020-03-11 13:29:37 -0400

    started challenge 6

--- s1c06.py
+++ s1c06.py
@@ -1,8 +1,22 @@
+import binascii as bin
+import xor from s1c02
 
-
+#import s1c06 as s
+#import binascii as b
+#s.findEditDist(b'this is a test', b'wokka wokka!!!')
 
 def editDistance():
+    # We want to find the length of our key, this can be any value from 2 to ~40 characters
+    # Compute the difference in bits between two strings
     pass
+
+def findEditDist(bytestr1, bytestr2):
+    # xor the two byte strings
+    res = xor(bytestr1,bytestr2)
+    # iterate through the byte literal to count the number of ones
+
+    # return the number of differing bits
+
 
 def solveS1C06():
     pass
